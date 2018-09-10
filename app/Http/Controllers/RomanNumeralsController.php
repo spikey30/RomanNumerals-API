0221577c313ab0a5d05a0bd668c2f5f471632b6a
alex irvin <alexirvin70@gmail.com>
2018-09-10 18:13:58 +0100

    changed the functions in the roman numerals controller to the three api endpoints

--- app/Http/Controllers/RomanNumeralsController.php
+++ app/Http/Controllers/RomanNumeralsController.php
@@ -2,83 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\RomanNumeralsConversion;
 use Illuminate\Http\Request;
 
 class RomanNumeralsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return "hello";
-    }
+
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Convert a decimal and store it in the db.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function convert(Request $request)
+    {   
+
+
+        //convert decimal
+
+        //check if conversion already exists
+
+        //increment if exists else store conversion
+
+        return "convert";
     }
 
     /**
-     * Display the specified resource.
+     * get latest (last 15).
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLatest()
+    {
+        $conversions = RomanNumeralsConversion::orderBy('updated_at', 'desc')->limit(15)->get();
+        return $conversions;
+        
+    }
+
+    /**
+     * get the top 10 numbers using the usage count .
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function getTop10()
     {
-        //
+        $conversions = RomanNumeralsConversion::orderBy('usage_count', 'asc')->limit(10)->get();
+        return $conversions;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
