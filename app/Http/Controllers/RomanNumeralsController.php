f722c90471888b2e07861c54adf9051fbb6e620c
alex irvin <alexirvin70@gmail.com>
2018-09-10 20:13:02 +0100

    updated the roman numerals conversion controller to have db functions and also the logic to add/update a conversion

--- app/Http/Controllers/RomanNumeralsController.php
+++ app/Http/Controllers/RomanNumeralsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\RomanNumeralsConversion;
 use Illuminate\Http\Request;
+use App\Http\Resources\RomanNumeralsResource;
 
 class RomanNumeralsController extends Controller
 {
@@ -15,17 +16,36 @@ class RomanNumeralsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function convert(Request $request)
+    public function convert($int)
     {   
-
-
+        
         //convert decimal
 
+
         //check if conversion already exists
+        $conversion = RomanNumeralsConversion::where('Numerals','=','hhh')->first();
 
         //increment if exists else store conversion
+        if ($conversion != null) {
+            //conversion already exists so update its usage_count
+            $updated_usage_count = $conversion->usage_count + 1;
+            $conversion->usage_count = $updated_usage_count;
+            $conversion->save();
+            
+            return new RomanNumeralsResource($conversion);
+        } else {
+            
+            $new_conversion = new RomanNumeralsConversion;
 
-        return "convert";
+            $new_conversion->integer = $int;
+            $new_conversion->usage_count = 1;
+            $new_conversion->Numerals = "hhh4";
+            $new_conversion->save();
+
+            return new RomanNumeralsResource($new_conversion);
+        }
+
+        
     }
 
     /**
@@ -36,20 +56,19 @@ class RomanNumeralsController extends Controller
     public function getLatest()
     {
         $conversions = RomanNumeralsConversion::orderBy('updated_at', 'desc')->limit(15)->get();
-        return $conversions;
+        return RomanNumeralsResource::collection($conversion);
         
     }
 
     /**
      * get the top 10 numbers using the usage count .
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function getTop10()
     {
-        $conversions = RomanNumeralsConversion::orderBy('usage_count', 'asc')->limit(10)->get();
-        return $conversions;
+        $conversions = RomanNumeralsConversion::orderBy('usage_count', 'desc')->limit(10)->get();
+        return RomanNumeralsResource::collection($conversions);
     }
 
 }
