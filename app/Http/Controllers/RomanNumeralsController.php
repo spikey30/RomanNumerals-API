02d436a2f2b3318acae377ac6f5afe7e558ad98b
alex irvin <alexirvin70@gmail.com>
2018-09-10 23:06:01 +0100

    changed the romanNumerals controller to take account of error codes and respond accordingly

--- app/Http/Controllers/RomanNumeralsController.php
+++ app/Http/Controllers/RomanNumeralsController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers;
 use App\RomanNumeralsConversion;
 use Illuminate\Http\Request;
 use App\Http\Resources\RomanNumeralsResource;
+use App\IntegerConversionInterface;
+use App\IntegerConversion;
 
 class RomanNumeralsController extends Controller
 {
 
+    private $IntegerConversion;
+    
+    public function __construct(IntegerConversion $IntegerConversion)
+    {
+        $this->IntegerConversion = $IntegerConversion;
+    }
 
     /**
      * Convert a decimal and store it in the db.
@@ -17,35 +25,43 @@ class RomanNumeralsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function convert($int)
-    {   
+    {
         
         //convert decimal
+        $conversion_result = $this->IntegerConversion->toRomanNumerals($int);
+        
+        // check if result has an error otherwise continue
+        if ($conversion_result == ">3999") {
+            return response()->json(['errors' => ['Integer' => ['The integer entered is more than 3999 ']]], 422);
+        }
+        
+        if ($conversion_result == "<1") {
+            return response()->json(['errors' => ['Integer' => ['The integer entered is less  than 1 ']]], 422);
+        }
 
 
         //check if conversion already exists
-        $conversion = RomanNumeralsConversion::where('Numerals','=','hhh')->first();
+        $existing_conversion = RomanNumeralsConversion::where('Numerals', '=', $conversion_result)->first();
 
         //increment if exists else store conversion
-        if ($conversion != null) {
+        if ($existing_conversion != null) {
             //conversion already exists so update its usage_count
-            $updated_usage_count = $conversion->usage_count + 1;
-            $conversion->usage_count = $updated_usage_count;
-            $conversion->save();
+            $updated_usage_count = $existing_conversion->usage_count + 1;
+            $existing_conversion->usage_count = $updated_usage_count;
+            $existing_conversion->save();
             
             return new RomanNumeralsResource($conversion);
         } else {
-            
+            //create new conversion
             $new_conversion = new RomanNumeralsConversion;
 
             $new_conversion->integer = $int;
             $new_conversion->usage_count = 1;
-            $new_conversion->Numerals = "hhh4";
+            $new_conversion->Numerals = $result;
             $new_conversion->save();
 
             return new RomanNumeralsResource($new_conversion);
         }
-
-        
     }
 
     /**
@@ -56,8 +72,7 @@ class RomanNumeralsController extends Controller
     public function getLatest()
     {
         $conversions = RomanNumeralsConversion::orderBy('updated_at', 'desc')->limit(15)->get();
-        return RomanNumeralsResource::collection($conversion);
-        
+        return RomanNumeralsResource::collection($conversions);
     }
 
     /**
@@ -70,5 +85,4 @@ class RomanNumeralsController extends Controller
         $conversions = RomanNumeralsConversion::orderBy('usage_count', 'desc')->limit(10)->get();
         return RomanNumeralsResource::collection($conversions);
     }
-
 }
